85fb712d766dbce3a71d270ac65b52ddc0cdc97f
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-11-26 01:50:35 +0530

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,69 @@
-import Image from "next/image";
+import Link from 'next/link'
+import React from 'react'
+import { AiOutlineGithub } from 'react-icons/ai'
+import { FaXTwitter } from 'react-icons/fa6'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className='min-h-screen w-screen font-display bg-white text-black flex flex-col text-sm'>
+      <Nav />
+     <div className='mx-auto'>
+       <DisplayCards />
+     </div>
+      <Footer />
     </div>
-  );
+  )
+}
+
+export default page
+
+const Nav = () => {
+  return (
+    <div className='fixed top-0 bg-white w-screen h-fit flex md:flex-row flex-col md:gap-0 gap-1 justify-between items-center px-5 py-3 uppercase'>
+      <Link href='/' className='hover:underline'>Designed.cards</Link>
+      <div>your ui library of ready to use cards</div>
+    </div>
+  )
+}
+
+const DisplayCards = () => {
+  return (
+    <div className='w-screen h-screen p-5 md:pt-25 pt-30 grid md:grid-cols-4 md:gap-20 gap-10 grid-cols-1 pb-20 overflow-y-scroll'>
+      <Card imgUrl='https://cdn.cosmos.so/2acebb6c-c04a-4442-b941-8d368d3ac807?format=jpeg' title='01' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/1a853e35-a7d4-4de2-8a68-ef46bd1b6a25?format=jpeg' title='02' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/c8483ef0-cd1e-466f-ba58-19f7c1b67eb6?format=jpeg' title='03' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/1563d902-2a41-46d3-ba0f-89f7cb8d40ab?format=jpeg' title='04' href='/' />
+       <Card imgUrl='https://cdn.cosmos.so/2acebb6c-c04a-4442-b941-8d368d3ac807?format=jpeg' title='01' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/1a853e35-a7d4-4de2-8a68-ef46bd1b6a25?format=jpeg' title='02' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/c8483ef0-cd1e-466f-ba58-19f7c1b67eb6?format=jpeg' title='03' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/1563d902-2a41-46d3-ba0f-89f7cb8d40ab?format=jpeg' title='04' href='/' />
+       <Card imgUrl='https://cdn.cosmos.so/2acebb6c-c04a-4442-b941-8d368d3ac807?format=jpeg' title='01' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/1a853e35-a7d4-4de2-8a68-ef46bd1b6a25?format=jpeg' title='02' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/c8483ef0-cd1e-466f-ba58-19f7c1b67eb6?format=jpeg' title='03' href='/' />
+      <Card imgUrl='https://cdn.cosmos.so/1563d902-2a41-46d3-ba0f-89f7cb8d40ab?format=jpeg' title='04' href='/' />
+    </div>
+  )
+}
+
+const Card = ({ imgUrl, title, href }: { imgUrl: string, title: string, href: string }) => {
+  return (
+    <Link href={href} className='flex flex-col gap-2 items-center'>
+      <div className='size-60 rounded-4xl border border-neutral-300 overflow-hidden hover:shadow-lg hover:shadow-neutral-200'>
+        <img src={imgUrl} alt='' className='w-full h-full object-cover' />
+      </div>
+      <div>{title}</div>
+    </Link>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div className='bg-white fixed w-screen bottom-0 px-5 py-3 text-xs uppercase flex md:flex-row flex-col-reverse justify-between items-center gap-1'>
+      <div>2025 designed.cards. All rights Reserved.</div>
+      <div className='flex items-center gap-3'>
+        <Link href='/'><FaXTwitter className='size-4 cursor-pointer' /></Link>
+        <Link href='/'><AiOutlineGithub className='size-4 cursor-pointer'  /></Link>
+      </div>
+    </div>
+  )
 }
